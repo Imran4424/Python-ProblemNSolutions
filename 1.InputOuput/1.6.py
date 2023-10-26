938a4e7a66df94a81926dd6525ce23706b446bde
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2023-10-27 00:41:03 +0600

    input output

---
+++ 1.InputOuput/1.6.py
@@ -0,0 +1,7 @@
+# 1.6. Write a program that read and display any character
+
+character = input("Enter a character: ")
+
+print("character you entered: ", character[0])
+
+print(f"{character[0]} entered", type(character[0]))
---
+++ 1.InputOuput/1.8.py
@@ -0,0 +1,5 @@
+# 1.8. Wrtie a program that read any character and display equivalent ASCII value
+
+character = input("enter the character: ")
+
+print("Equivalent ASCII value of given character is: ", ord(character[0]))
